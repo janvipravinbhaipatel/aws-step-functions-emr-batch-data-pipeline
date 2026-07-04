76babd9acc34f91e3db7ec2c8e4ccb4b5ae8953c
janvipravinbhaipatel <158079903+janvipravinbhaipatel@users.noreply.github.com>
2026-07-03 21:48:27 -0400

    Add PySpark processing script

---
+++ script.py
@@ -0,0 +1,24 @@
+import sys
+import time
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+bucket_name = "<YOUR_S3_BUCKET_NAME>"
+
+spark = SparkSession.builder.appName("stock-summary").getOrCreate()
+
+stockDF = spark.read.option("header", True).csv("s3://" + bucket_name + "/data/")
+
+stockDF.createOrReplaceTempView("stock_data_view")
+
+StockSummaryDF = spark.sql("""
+SELECT `Trade_Date`, `Ticker`, `Close`
+FROM stock_data_view
+WHERE Volume > 100000
+ORDER BY Close DESC
+""")
+
+StockSummaryDF.write.mode("overwrite").parquet("s3://" + bucket_name + "/output/")
+
+spark.stop()
